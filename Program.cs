b0bd192a8d048bb5a47f93a98233125dbc6c38e0
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-16 21:53:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Fixer
+{
+    public static List<int> PipeFix(List<int> numbers)
+    {
+        return Enumerable.Range(numbers.First(), numbers.Last() - numbers.First() + 1).ToList();
+    }
+}
+13 similar code variations are gr
